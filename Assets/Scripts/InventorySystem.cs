d59d8d80bb51fd1e8ee73a12c578f52bd2d62b7c
Edy <101060269+EdyX11@users.noreply.github.com>
2024-03-04 20:44:19 +0200

    invetory system updated
    added a 21 slot list which is parsed with a counter to check for available slots to be filled by collected items
    condition to check if the inventory is full, when child counter is >21 , resultin into 21 childs, one child for each of the 21 slots
    search for nextemptyslot by using the list created previously , if slot has child = 0 it is available to be filled

--- Assets/Scripts/InventorySystem.cs
+++ Assets/Scripts/InventorySystem.cs
@@ -7,7 +7,20 @@ public class InventorySystem : MonoBehaviour
     public static InventorySystem Instance { get; set; }
 
     public GameObject inventoryScreenUI;
+
+    public List<GameObject> slotList = new List<GameObject>();
+
+    public List<string> itemList = new List<string>();
+
+    private GameObject itemToAdd;
+
+    private GameObject whatSlotToEquip;
+
+
     public bool isOpen;
+
+    //public bool isFull;
+
 
 
     private void Awake()
@@ -26,7 +39,26 @@ public class InventorySystem : MonoBehaviour
     void Start()
     {
         isOpen = false;
+        //isFull = false;
+
+        PopulateSlotList();
+
+
     }
+
+    private void PopulateSlotList()
+    {
+        foreach (Transform child in inventoryScreenUI.transform)
+        {
+            if (child.CompareTag("Slot"))
+            {
+
+                slotList.Add(child.gameObject);
+            }
+        }
+    }
+
+
 
 
     void Update()
@@ -35,7 +67,7 @@ public class InventorySystem : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.I) && !isOpen)
         {
 
-            Debug.Log("i is pressed");
+            Debug.Log("I is pressed");
             inventoryScreenUI.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
             isOpen = true;
@@ -48,4 +80,61 @@ public class InventorySystem : MonoBehaviour
             isOpen = false;
         }
     }
+
+    public void AddToInventory(string itemName)
+    {
+    
+            whatSlotToEquip = FindNextEmptySlot();
+
+            itemToAdd = Instantiate(Resources.Load<GameObject>(itemName), whatSlotToEquip.transform.position, whatSlotToEquip.transform.rotation); // items , position , rotation we instatiate at
+            itemToAdd.transform.SetParent(whatSlotToEquip.transform);
+
+            itemList.Add(itemName);
+        
+    }
+    private GameObject FindNextEmptySlot()
+    {
+        foreach (GameObject slot in slotList)
+        {
+
+            if (slot.transform.childCount == 0) // if slot has no children -> slot is available to be filled 
+            {
+
+                return slot;
+            }
+        }
+
+        return new GameObject();
+
+    }
+
+    public bool CheckIfFull()
+    {
+        int counter = 0;
+
+        foreach (GameObject slot in slotList)
+        {
+            if (slot.transform.childCount > 0)
+            {
+
+                counter += 1;
+
+            }
+        }
+
+        if (counter == 21)
+        {
+            return true;
+        }
+        else
+        {
+
+            return false;
+        }
+
+    }
+
+
+
+
 }
