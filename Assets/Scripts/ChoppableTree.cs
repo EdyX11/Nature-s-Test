c3e5095aa0ac873da9d1a2e0d2c6dde2303b4571
Edy <101060269+EdyX11@users.noreply.github.com>
2024-03-15 12:17:46 +0200

    tree resource created

---
+++ Assets/Scripts/ChoppableTree.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(BoxCollider))]
+public class ChoppableTree : MonoBehaviour
+{
+    public bool playerInRange;
+    public bool canBeChopped;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            playerInRange = false;
+        }
+
+
+    }
+
+}
