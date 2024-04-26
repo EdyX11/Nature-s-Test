32e780fd9f90fed51b0da5d67997fc94beb9facf
Edy <101060269+EdyX11@users.noreply.github.com>
2024-04-26 13:55:30 +0300

    basic hit as for the animals functional on the zombie

--- Assets/Scripts/EnemyNPC.cs
+++ Assets/Scripts/EnemyNPC.cs
@@ -1,22 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyNPC : MonoBehaviour
 {
+    private NavMeshAgent navAgent;
+    public string enemyName;
+    public bool playerInRange;
+    public bool IsAlreadyBeingAttacked;
+    private bool isHitRecovering = false;
+    [SerializeField] private float hitCooldown = 2.0f;
+
+    
     [SerializeField] private int HP = 100;
     private Animator animator;
-    public bool playerInRange;
+   
     public bool isDead;
-    public string enemyName;
+    
 
-    private bool isHitRecovering = false;
-    private float hitCooldown = 2.0f;
+    
     [Header("Sounds")]
     [SerializeField] AudioSource soundChannel;
     [SerializeField] AudioClip enemyHit;
     [SerializeField] AudioClip enemyDie;
-    public bool IsAlreadyBeingAttacked;
+   
+
+  
 
 
 
@@ -25,19 +35,31 @@ public class EnemyNPC : MonoBehaviour
     {
 
         animator = GetComponent<Animator>();
-
+        navAgent = GetComponent<NavMeshAgent>();
     }
     
     public void TakeDamage(int damageAmount)
     {
 
-        HP-=damageAmount;   
+        HP-=damageAmount;
 
-        if(HP <= 0 ) 
+        if (HP <= 0)
         {
-            animator.SetTrigger("DIE");
+            if (!isDead)
+            {
+                isDead = true;
+                int randomValue = Random.Range(0, 2); // 0 or 1
+                if (randomValue == 0)
+                {
 
-        
+                    animator.SetTrigger("DIE1");
+                }
+                else
+                {
+
+                    animator.SetTrigger("DIE2");
+                }
+            }
         }
         else
         {
@@ -46,6 +68,26 @@ public class EnemyNPC : MonoBehaviour
         }
 
     }
+
+    private void Update()
+    {
+        if(navAgent.velocity.magnitude > 0.1f)// moving start =>start walk
+        
+        
+        {
+
+            animator.SetBool("isWalking",true);
+   
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
